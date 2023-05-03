5a864ff9d14627e0513390e04fe2024a86e327b2
yury <yury.vasyutinskiy@gmail.com>
2023-05-03 09:29:45 +0200

    Fix javadoc Add sonatype required fields to pom

--- server/src/main/java/org/falland/grpc/longlivedstreams/server/ServerServiceDefinitionWrapper.java
+++ server/src/main/java/org/falland/grpc/longlivedstreams/server/ServerServiceDefinitionWrapper.java
@@ -7,7 +7,7 @@ import io.grpc.ServerServiceDefinition;
  * The interface that wraps gRPC server service definition
  * This interface is useful when you want to wrap your {@link io.grpc.BindableService} in an interceptor
  *
- * An example for such an intercepted service can be {@link AbstractSubscriptionAwareComponent}.
+ * An example for such an intercepted service can be {@link AbstractSubscriptionAware}.
  * It uses {@link AddressInterceptor} to propagate client address to gRPC call
  */
 public interface ServerServiceDefinitionWrapper {
