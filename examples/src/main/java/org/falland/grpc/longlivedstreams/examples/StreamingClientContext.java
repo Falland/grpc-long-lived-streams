61280792e59e69ca698f9b19a31f9d92c60ebcab
yury <yury.vasyutinskiy@gmail.com>
2023-10-26 15:04:44 +0200

    Add executor option to managed channel creation

--- examples/src/main/java/org/falland/grpc/longlivedstreams/examples/StreamingClientContext.java
+++ examples/src/main/java/org/falland/grpc/longlivedstreams/examples/StreamingClientContext.java
@@ -2,15 +2,20 @@ package org.falland.grpc.longlivedstreams.examples;
 
 import org.falland.grpc.longlivedstreams.client.ClientContext;
 
+import java.util.concurrent.Executor;
+
 public class StreamingClientContext implements ClientContext {
     private final String hostName;
     private final String clientName;
     private final int port;
 
-    public StreamingClientContext(String hostName, String clientName, int port) {
+    private final Executor executor;
+
+    public StreamingClientContext(String hostName, String clientName, int port, Executor executor) {
         this.hostName = hostName;
         this.clientName = clientName;
         this.port = port;
+        this.executor = executor;
     }
 
     @Override
@@ -24,7 +29,12 @@ public class StreamingClientContext implements ClientContext {
     }
 
     @Override
-    public String getClientName() {
+    public Executor executor() {
+        return executor;
+    }
+
+    @Override
+    public String clientName() {
         return clientName;
     }
 
--- examples/src/main/java/org/falland/grpc/longlivedstreams/examples/apps/FullFlowServerStreamingClient.java
+++ examples/src/main/java/org/falland/grpc/longlivedstreams/examples/apps/FullFlowServerStreamingClient.java
@@ -1,17 +1,21 @@
 package org.falland.grpc.longlivedstreams.examples.apps;
 
 import com.falland.gprc.longlivedstreams.proto.helloworld.v1.World;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.google.protobuf.ByteString;
 import org.falland.grpc.longlivedstreams.client.ClientContext;
 import org.falland.grpc.longlivedstreams.examples.GrpcServer;
 import org.falland.grpc.longlivedstreams.examples.StreamingClient;
 import org.falland.grpc.longlivedstreams.examples.StreamingClientContext;
 import org.falland.grpc.longlivedstreams.examples.StreamingService;
 import org.falland.grpc.longlivedstreams.examples.WorldUpdateProcessor;
-import com.google.protobuf.ByteString;
 
 import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import static org.falland.grpc.longlivedstreams.examples.apps.Payload.payload;
 import static org.falland.grpc.longlivedstreams.examples.apps.PortUtils.findFreePort;
@@ -19,13 +23,18 @@ import static org.falland.grpc.longlivedstreams.examples.apps.PortUtils.findFree
 public class FullFlowServerStreamingClient {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        ThreadFactory factory = new ThreadFactoryBuilder()
+                .setNameFormat("full-flow-test-client-worker-%s")
+                .setDaemon(false)
+                .build();
+        ExecutorService clientExecutor = Executors.newSingleThreadScheduledExecutor(factory);
         int port = findFreePort();
         StreamingService streamingService = new StreamingService(10_000, Duration.ofMillis(1));
         GrpcServer server = new GrpcServer(port, List.of(streamingService.getGrpcService()));
         server.start();
 
         WorldUpdateProcessor updateProcessor = new WorldUpdateProcessor();
-        ClientContext clientContext = new StreamingClientContext("localhost", "testClient", port);
+        ClientContext clientContext = new StreamingClientContext("localhost", "testClient", port, clientExecutor);
         StreamingClient client = new StreamingClient(clientContext, List.of(updateProcessor), Duration.ofNanos(10), false);
         client.start();
         Thread.sleep(1000);
@@ -48,5 +57,6 @@ public class FullFlowServerStreamingClient {
         System.out.println("Received messages total : " + updateProcessor.getMessages().size());
         server.stop();
         client.stop();
+        clientExecutor.shutdown();
     }
 }
